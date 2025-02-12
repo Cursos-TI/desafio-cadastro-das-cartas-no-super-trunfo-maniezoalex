21d507fee82572f6e82f8434dead75b17e609aa0
Alex Maniezo <89463891+maniezoalex@users.noreply.github.com>
2025-02-12 21:26:10 +0000

    novo commit

--- CartasSuperTrunfo_nivel_basico.c
+++ CartasSuperTrunfo_nivel_basico.c
@@ -5,7 +5,6 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
     //definindo as variáveis que serão usadas para este módulo básico
