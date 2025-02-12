62f42fb571fdcbfe5d8dd03e1afad6f2215235c3
Alex Maniezo <89463891+maniezoalex@users.noreply.github.com>
2025-02-12 21:16:40 +0000

    commit codigo necessário para programa

--- CartasSuperTrunfo_nivel_basico.c
+++ CartasSuperTrunfo_nivel_basico.c
@@ -66,17 +66,6 @@ int main() {
     printf("\nNumero de pontos turisticos: %d", numero_pontos_turisticos);
     printf("\nPIB: %.2f", PIB);
     printf("\nArea em Km²: %.2fKm²", area_em_km);    
-
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
+    // fim do código
     return 0;    
 }
